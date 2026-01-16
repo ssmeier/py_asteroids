21ffc9d56e18ff96b8858fa2e815a3046c3f9a5c
ssmeier <ssmeier1@gmail.com>
2026-01-15 19:32:02 -0500

    added game_clock and delta-time variable

--- main.py
+++ main.py
@@ -7,6 +7,8 @@ from logger import log_state
 def main():
     game_run = True
     pygame.init()
+    game_clock = pygame.time.Clock()
+    dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
     print(f"Screen width: {SCREEN_WIDTH}")
@@ -18,6 +20,9 @@ def main():
                 return
         screen.fill('black')
         pygame.display.flip()
+        clock_time = game_clock.tick(60)
+        dt = clock_time/1000
+       
 
     
 
