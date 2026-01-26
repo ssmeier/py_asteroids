b0d87e64d34ce063cd32533eef50ecde50dd9a84
ssmeier <ssmeier1@gmail.com>
2026-01-25 19:27:53 -0500

    added simple collision logic, updated git ignore

--- circleshape.py
+++ circleshape.py
@@ -20,3 +20,11 @@ class CircleShape(pygame.sprite.Sprite):
     def update(self, dt):
         # must override
         pass
+
+    def collides_with(self, other):
+        does_collide = False
+        distance = self.position.distance_to(other.position)
+        combined_radius = self.radius + other.radius
+        if distance <= combined_radius:
+            does_collide = True
+        return does_collide
--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import pygame
 from constants import *
-from logger import log_state
+from logger import log_state, log_event
 from player import Player
 from asteroid import Asteroid
 from asteroidfield import *
